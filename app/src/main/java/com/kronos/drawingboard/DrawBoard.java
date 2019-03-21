93ca2c6ee5d3356b7ace013aa62b04e3fd5c05d7
religiones <1963024305@qq.com>
2019-03-21 17:59:34 +0800

    finish save function

--- app/src/main/java/com/kronos/drawingboard/DrawBoard.java
+++ app/src/main/java/com/kronos/drawingboard/DrawBoard.java
@@ -4,19 +4,41 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.AnticipateInterpolator;
 import android.view.animation.OvershootInterpolator;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
 import com.getbase.floatingactionbutton.FloatingActionsMenu;
 import com.ogaclejapan.arclayout.ArcLayout;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
+
 import me.panavtec.drawableview.DrawableView;
 import me.panavtec.drawableview.DrawableViewConfig;
+
+import static android.graphics.Bitmap.createBitmap;
 
 public class DrawBoard extends AppCompatActivity {
     private FloatingActionsMenu toolMenu = null;
@@ -37,7 +59,7 @@ public class DrawBoard extends AppCompatActivity {
         setToolsAction(m_Tools);
     }
 
-    protected void init(){
+    protected void init() {
         m_Draw = (DrawableView) findViewById(R.id.paintView);
         configPen = new DrawableViewConfig();
         configPen.setStrokeColor(getResources().getColor(android.R.color.black));
@@ -60,21 +82,20 @@ public class DrawBoard extends AppCompatActivity {
         arcLayout = (ArcLayout) findViewById(R.id.arc_layout);
     }
 
-    protected void setTools(Tools tools){
+    protected void setTools(Tools tools) {
         toolMenu = (FloatingActionsMenu) findViewById(R.id.tools);
-        toolMenu.addButton(tools.setToolStyle(tools.getPen(),getDrawable(R.drawable.pen)));
-        toolMenu.addButton(tools.setToolStyle(tools.getEraser(),getDrawable(R.drawable.eraser)));
-        toolMenu.addButton(tools.setToolStyle(tools.getUndo(),getDrawable(R.drawable.undo)));
-        toolMenu.addButton(tools.setToolStyle(tools.getSelectColor(),getDrawable(R.drawable.color)));
-        toolMenu.addButton(tools.setToolStyle(tools.getSave(),getDrawable(R.drawable.save)));
-        toolMenu.addButton(tools.setToolStyle(tools.getDelete(),getDrawable(R.drawable.delete)));
+        toolMenu.addButton(tools.setToolStyle(tools.getPen(), getDrawable(R.drawable.pen)));
+        toolMenu.addButton(tools.setToolStyle(tools.getEraser(), getDrawable(R.drawable.eraser)));
+        toolMenu.addButton(tools.setToolStyle(tools.getUndo(), getDrawable(R.drawable.undo)));
+        toolMenu.addButton(tools.setToolStyle(tools.getSelectColor(), getDrawable(R.drawable.color)));
+        toolMenu.addButton(tools.setToolStyle(tools.getSave(), getDrawable(R.drawable.save)));
+        toolMenu.addButton(tools.setToolStyle(tools.getDelete(), getDrawable(R.drawable.delete)));
     }
 
-    protected void setToolsAction(final Tools tools){
+    protected void setToolsAction(final Tools tools) {
         tools.getPen().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(DrawBoard.this,"pen",Toast.LENGTH_SHORT).show();
                 /* use pen */
                 m_Draw.setConfig(configPen);
             }
@@ -82,30 +103,28 @@ public class DrawBoard extends AppCompatActivity {
         tools.getPen().setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                setPenSize(v,configPen);
+                setPenSize(v, configPen);
                 return false;
             }
         });
         tools.getEraser().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(DrawBoard.this,"eraser",Toast.LENGTH_SHORT).show();
                 /* use eraser */
                 m_Draw.setConfig(configEra);
             }
         });
         tools.getEraser().setOnLongClickListener(new View.OnLongClickListener() {
-                /* set eraser size */
+            /* set eraser size */
             @Override
             public boolean onLongClick(View v) {
-                setPenSize(v,configEra);
+                setPenSize(v, configEra);
                 return false;
             }
         });
         tools.getUndo().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(DrawBoard.this,"undo",Toast.LENGTH_SHORT).show();
                 /* use undo */
                 m_Draw.undo();
             }
@@ -113,40 +132,69 @@ public class DrawBoard extends AppCompatActivity {
         tools.getSelectColor().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(DrawBoard.this,"selectColor",Toast.LENGTH_SHORT).show();
                 /* use selectColor */
+                ColorPickerDialogBuilder
+                        .with(DrawBoard.this)
+                        .setTitle("Choose color")
+                        .wheelType(ColorPickerView.WHEEL_TYPE.CIRCLE)
+                        .density(6)
+                        .setPositiveButton("ok", new ColorPickerClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+                                configPen.setStrokeColor(selectedColor);
+                                m_Draw.setConfig(configPen);
+                            }
+                        })
+                        .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .build()
+                        .show();
             }
         });
         tools.getSave().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(DrawBoard.this,"save",Toast.LENGTH_SHORT).show();
                 /* use save */
+                String[] PERMISSIONS = {
+                        "android.permission.READ_EXTERNAL_STORAGE",
+                        "android.permission.WRITE_EXTERNAL_STORAGE",
+                        "android.permission.MOUNT_FORMAT_FILESYSTEMS"};
+                if (ContextCompat.checkSelfPermission(DrawBoard.this, "android.permission.WRITE_EXTERNAL_STORAGE") != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(DrawBoard.this, PERMISSIONS, 1);
+                }
+                try {
+                    SavePhoto savePhoto = new SavePhoto(DrawBoard.this);
+                    m_Draw.setBackgroundColor(Color.WHITE);
+                    savePhoto.SaveBitmapFromView(m_Draw,DrawBoard.this);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
             }
         });
         tools.getDelete().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(DrawBoard.this,"delete",Toast.LENGTH_SHORT).show();
                 /* use delete */
                 m_Draw.clear();
             }
         });
     }
 
-    protected void showPenSize(View v){
-        if (v.isSelected()){
+    protected void showPenSize(View v) {
+        if (v.isSelected()) {
             /* hide */
             hidePenMenu();
-        }
-        else {
+        } else {
             /* show */
             showPenMenu();
         }
     }
 
-    protected void setPenSize(View v, final DrawableViewConfig config){
-        Toast.makeText(DrawBoard.this,"set pen size",Toast.LENGTH_SHORT).show();
+    protected void setPenSize(View v, final DrawableViewConfig config) {
+        Toast.makeText(DrawBoard.this, "set pen size", Toast.LENGTH_SHORT).show();
         /* set pen size */
         toolMenu.toggle();
         showPenSize(v);
@@ -154,7 +202,7 @@ public class DrawBoard extends AppCompatActivity {
             arcLayout.getChildAt(i).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Button btn = (Button)v;
+                    Button btn = (Button) v;
                     config.setStrokeWidth(Float.valueOf(btn.getText().toString()));
                     m_Draw.setConfig(config);
                     hidePenMenu();
@@ -164,7 +212,7 @@ public class DrawBoard extends AppCompatActivity {
         }
     }
 
-    protected void hidePenMenu(){
+    protected void hidePenMenu() {
         List<Animator> animatorList = new ArrayList<>();
         for (int i = arcLayout.getChildCount() - 1; i >= 0; i--) {
             animatorList.add(createHideItemAnimator(arcLayout.getChildAt(i)));
@@ -184,7 +232,7 @@ public class DrawBoard extends AppCompatActivity {
         animSet.start();
     }
 
-    protected void showPenMenu(){
+    protected void showPenMenu() {
         menuLayout.setVisibility(View.VISIBLE);
         List<Animator> animatorList = new ArrayList<>();
         for (int i = 0, len = arcLayout.getChildCount(); i < len; i++) {
@@ -235,3 +283,4 @@ public class DrawBoard extends AppCompatActivity {
         return anim;
     }
 }
+
---
+++ app/src/main/java/com/kronos/drawingboard/SavePhoto.java
@@ -0,0 +1,90 @@
+package com.kronos.drawingboard;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.ParseException;
+
+public class SavePhoto {
+        Context context;
+
+        public SavePhoto(Context context) {
+            this.context = context;
+        }
+
+    public void SaveBitmapFromView(View view, Context context) throws ParseException {
+        int w = view.getWidth();
+        int h = view.getHeight();
+        Bitmap bmp = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bmp);
+        view.layout(0, 0, w, h);
+        view.draw(c);
+        // 缩小图片
+        Matrix matrix = new Matrix();
+        matrix.postScale(0.5f,0.5f);
+        bmp = Bitmap.createBitmap(bmp,0,0,bmp.getWidth(),bmp.getHeight(),matrix,true);
+        String path = saveBitmap(bmp,System.currentTimeMillis() + ".JPEG");
+        refreshFile(path, context);
+    }
+
+
+    public String saveBitmap(Bitmap bitmap, String bitName){
+        String fileName ;
+        File file, appDir;
+        appDir = new File(Environment.getExternalStorageDirectory().getAbsolutePath(),"DrawBoard");
+        if (!appDir.exists()){
+                appDir.mkdir();
+        }
+        fileName = appDir.getPath()+"/"+bitName;
+        file = new File(fileName);
+        if(file.exists()){
+            file.delete();
+        }
+        FileOutputStream out;
+        try{
+            out = new FileOutputStream(file);
+            if(bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out))
+            {
+                out.flush();
+                out.close();
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+
+        }
+        return file.getAbsolutePath();
+    }
+
+
+    public void refreshFile(String filePath, Context mContext) {
+        File file = new File(filePath);
+        MimeTypeMap mtm = MimeTypeMap.getSingleton();
+        MediaScannerConnection.scanFile(mContext, new String[] { file.toString() }, new String[] { mtm.getMimeTypeFromExtension(file.toString().substring(file.toString().lastIndexOf(".")+1)) },
+                new MediaScannerConnection.OnScanCompletedListener() {
+                    @Override
+                    public void onScanCompleted(final String path, final Uri uri) {
+                        Log.d("", "刷新完毕");
+                    }
+                });
+    }
+
+}
+
